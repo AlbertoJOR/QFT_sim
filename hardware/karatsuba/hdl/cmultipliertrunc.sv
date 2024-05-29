1766f39fb5f1e7e0240da9dc5e690ae97c945a57
AlbertoJOR <albertojortizr@gmail.com>
2024-05-29 08:26:14 -0600

    error sing

--- hardware/karatsuba/hdl/cmultipliertrunc.sv
+++ hardware/karatsuba/hdl/cmultipliertrunc.sv
@@ -1,13 +1,13 @@
 `timescale 1ns/1ps
-module cmultiplier #(
+module cmultipliertrunc #(
     parameter integer DATA_W = 32
 ) (
     input logic [DATA_W - 1 :0] A_real,
     input logic [DATA_W - 1 :0] A_img,
     input logic [DATA_W - 1 :0] B_real,
     input logic [DATA_W - 1 :0] B_img,
-    output logic [DATA_W * 2 : 0] S_real,
-    output logic [DATA_W * 2 : 0] S_img
+    output logic [DATA_W * 2 - 1: 0] S_real,
+    output logic [DATA_W * 2 - 1: 0] S_img
     // output logic [DATA_W * 2: 0] S_real,
     // output logic [DATA_W * 2: 0] S_img
 );
@@ -58,7 +58,13 @@ module cmultiplier #(
         .S(res_img)
     );
 
-    assign S_real = res_real;
-    assign S_img = res_img;
+    assign S_real = res_real[DATA_W * 2 - 1: 0];
+    assign S_img = res_img[DATA_W * 2 - 1: 0];
+    // Revisa si es necesario eliminar ese bit.
+    // assign S_real = res_real[DATA_W * 2: 0];
+    // assign S_img = res_img[DATA_W * 2: 0];
+
+    
+
     
 endmodule
