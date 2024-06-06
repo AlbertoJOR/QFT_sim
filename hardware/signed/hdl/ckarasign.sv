51c9c7eedc676f8799b01972b7f758ad88e856b6
AlbertoJOR <84932822+AlbertoJOR@users.noreply.github.com>
2024-06-06 10:29:24 -0600

    ckara

---
+++ hardware/signed/hdl/ckarasign.sv
@@ -0,0 +1,81 @@
+`timescale 1ns/1ps
+module ckarasign #(
+    parameter integer DATA_W = 32
+) (
+    input logic [DATA_W - 1 :0] A_real,
+    input logic [DATA_W - 1 :0] A_img,
+    input logic [DATA_W - 1 :0] B_real,
+    input logic [DATA_W - 1 :0] B_img,
+    output logic [DATA_W - 1 : 0] S_real,
+    output logic [DATA_W - 1 : 0] S_img,
+    output logic overflow 
+);
+    wire [DATA_W - 1 :0] real_part, img_part, Asum, Bsum, AB, img_part_inv, neg_part;
+    wire over[7 : 0];
+    
+
+    multiplierkarasign #(.DATA_W(DATA_W)) mult1 (
+        .A(A_real),
+        .B(B_real),
+        .S(real_part),
+        .overflow(over[0])
+    );
+
+
+    multiplierkarasign #(.DATA_W(DATA_W)) mult2 (
+        .A(A_img),
+        .B(B_img),
+        .S(img_part),
+        .overflow(over[1])
+    );
+
+    assign img_part_inv = {~img_part[DATA_W-1], img_part[DATA_W-2: 0]};
+
+    wire [DATA_W -1  :0] res_real, res_img;
+
+    addersign #(.DATA_W(DATA_W )) addreal(
+        .A(real_part),
+        .B(img_part_inv),
+        .S(S_real),
+        .overflow(over[2])
+    );
+
+    addersign #(.DATA_W(DATA_W )) adderA(
+        .A(A_img),
+        .B(A_real),
+        .S(Asum),
+        .overflow(over[3])
+    );
+
+    addersign #(.DATA_W(DATA_W )) adderB(
+        .A(B_img),
+        .B(B_real),
+        .S(Bsum),
+        .overflow(over[4])
+    );
+
+    multiplierkarasign #(.DATA_W(DATA_W)) mult3 (
+        .A(Asum),
+        .B(Bsum),
+        .S(AB),
+        .overflow(over[5])
+    );
+
+    addersign #(.DATA_W(DATA_W )) sumneg(
+        .A({~real_part[DATA_W-1], real_part[DATA_W-2:0]}),
+        .B({~img_part[DATA_W-1], real_part[DATA_W-2:0]}),
+        .S(neg_part),
+        .overflow(over[6])
+    );
+
+    addersign #(.DATA_W(DATA_W )) adderimg(
+        .A(neg_part),
+        .B(AB),
+        .S(S_img),
+        .overflow(over[7])
+    );
+
+
+    assign overflow = over[0] | over[1] | over[2] | over[3] | over[4] | over[5] | over[6] | over[7];
+    
+endmodule
--- hardware/signed/hdl/cmultipliersign.sv
+++ hardware/signed/hdl/cmultipliersign.sv
@@ -11,34 +11,34 @@ module cmultipliersign #(
     output logic overflow 
 );
     wire [DATA_W - 1 :0] real_part, img_part, ArBi, BrAi, img_part_inv;
-    wire o_real, o_img, o_AB, o_BA, o_addr, o_addi;
+    wire [5:0] over;
 
     multiplierkarasign #(.DATA_W(DATA_W)) mult1 (
         .A(A_real),
         .B(B_real),
         .S(real_part),
-        .overflow(o_real)
+        .overflow(over[0])
     );
 
     multiplierkarasign #(.DATA_W(DATA_W)) mult2 (
         .A(A_real),
         .B(B_img),
         .S(ArBi),
-        .overflow(o_AB)
+        .overflow(over[1])
     );
 
     multiplierkarasign #(.DATA_W(DATA_W)) mult3 (
         .A(B_real),
         .B(A_img),
         .S(BrAi),
-        .overflow(o_BA)
+        .overflow(over[2])
     );
 
     multiplierkarasign #(.DATA_W(DATA_W)) mult4 (
         .A(A_img),
         .B(B_img),
         .S(img_part),
-        .overflow(o_img)
+        .overflow(over[3])
     );
 
     assign img_part_inv = {~img_part[DATA_W-1], img_part[DATA_W-2: 0]};
@@ -49,18 +49,18 @@ module cmultipliersign #(
         .A(real_part),
         .B(img_part_inv),
         .S(res_real),
-        .overflow(o_addr)
+        .overflow(over[4])
     );
 
     addersign #(.DATA_W(DATA_W )) addimg(
         .A(ArBi),
         .B(BrAi),
         .S(res_img),
-        .overflow(o_addi)
+        .overflow(over[5])
     );
 
     assign S_real = res_real;
     assign S_img = res_img;
-    assign overflow = o_real | o_img | o_AB| o_BA | o_addi | o_addr;
+    assign overflow = | over;
     
 endmodule
